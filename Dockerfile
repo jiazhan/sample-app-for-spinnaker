fc14f74b10792d045e4e3254dc7b21b3000f6927
EC2 Default User <ec2-user@ip-172-31-18-3.us-west-1.compute.internal>
2021-12-04 09:08:18 +0000

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM ruby:2.7
 
+# Dockerfile
+FROM ruby:2.7-alpine
 RUN gem install sinatra
-
 COPY app.rb /
-
 CMD ["ruby", "app.rb"]
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
+# app.rb
 require "sinatra"
-
 set :bind, "0.0.0.0"
 
 get "/" do
-  raise "Whoops, something went wrong"
+ "Hello, Spinnaker!"
 end
